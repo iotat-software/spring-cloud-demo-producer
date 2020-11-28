c288a4fd95eebf4cecaa440648d9c47de3fc8514
pangyuworld <a904237539@qq.com>
2020-11-28 17:24:47 +0800

    trace

---
+++ producer-demo-service/src/main/java/cn/iotat/producer/handler/GlobalTraceHandler.java
@@ -0,0 +1,31 @@
+package cn.iotat.producer.handler;
+
+import brave.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalTraceHandler implements ResponseBodyAdvice<Object> {
+    @Autowired
+    private Tracer tracer;
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        HttpHeaders headers = serverHttpResponse.getHeaders();
+        headers.add("Trace", tracer.currentSpan().context().traceIdString());
+        return o;
+    }
+}
